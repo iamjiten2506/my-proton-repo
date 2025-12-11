4a1c32bb392986913d87bc6f0878ee03d8ef1a8d
iamjiten2506 <iamjiten@amazon.com>
2025-12-11 19:01:13 +0000

    Create variables.tf

---
+++ environments/test-env/variables.tf
@@ -0,0 +1,16 @@
+variable "environment_name" {
+  description = "Name of the Proton environment"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}

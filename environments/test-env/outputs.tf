3ebc5d2613102a2d380195a0eba84c253a320ddf
iamjiten2506 <iamjiten@amazon.com>
2025-12-11 19:01:42 +0000

    Create outputs.tf

---
+++ environments/test-env/outputs.tf
@@ -0,0 +1,14 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  description = "IDs of the subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "environment_name" {
+  description = "Environment name"
+  value       = var.environment_name
+}
